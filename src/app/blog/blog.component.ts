62ad0c0da71b672599361fc2211d12c6760c895e
Ascyt <contact@ascyt.com>
2025-06-30 10:32:45 +0200

    Added meta and title setting to blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
@@ -8,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.scss'
 })
 export class BlogComponent {
+  public constructor(private titleService: Title, private metaService: Meta, private activatedRoute: ActivatedRoute, private router:Router) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.url.subscribe(url => {
+      this.titleService.setTitle('Ascyt • Blog');
+      this.metaService.updateTag({property: 'og:title', content: 'Ascyt • Blog'});
+      this.metaService.updateTag({property: 'og:url', content: 'https://ascyt.com' + this.router.url});
+      this.metaService.updateTag({property: 'og:description', content: 'Blog posts and articles from Ascyt (Filip Schauer).'});
+    });
+  }
 }
