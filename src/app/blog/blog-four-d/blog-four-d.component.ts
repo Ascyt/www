37c3c8b979a7050084383dfa427b42195cb6938d
Ascyt <contact@ascyt.com>
2025-07-01 09:26:56 +0200

    Updated blog 4d

--- src/app/blog/blog-four-d/blog-four-d.component.ts
+++ src/app/blog/blog-four-d/blog-four-d.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-blog-four-d',
   standalone: true,
-  imports: [],
+  imports: [NgbModule],
   templateUrl: './blog-four-d.component.html',
   styleUrl: './blog-four-d.component.scss'
 })
