8cee43218a034fcdb0f4d1957ef04243c031335b
Ascyt <account@ascyt.com>
2026-01-11 01:35:49 +0100

    Added meta tags for german version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent {
 
   private cycleRoutes:string[] = [this.homeRoute, this.contactRoute, this.projectsRoute, this.aboutRoute];
 
-  constructor(public router:Router, public themeSwitcher:ThemeSwitcherService) {
+  constructor(public router:Router, public themeSwitcher:ThemeSwitcherService, private metaService: Meta, private activatedRoute: ActivatedRoute) {
     this.updateTheme();
   }
 
@@ -73,45 +73,94 @@ export class AppComponent {
     }
   }
 
+  
+  ngOnInit(): void {
+    if (this.language === 'de') {
+      this.metaService.updateTag({property: 'og:url', content: 'https://de.ascyt.com/'});
+      this.metaService.updateTag({property: 'og:image:alt', content: 'Logo von Ascyt'});
+      this.metaService.updateTag({property: 'og:description', content: 'Die persönliche Webseite von Ascyt (Filip Schauer), einem jungen Entwickler aus Österreich.'});
+      this.metaService.updateTag({property: 'og:locale', content: 'de_AT'});
+      this.metaService.updateTag({property: 'og:locale', content: 'de_DE'});
+      this.metaService.updateTag({property: 'og:profile:gender', content: 'männlich'});
+    }
+  };
+
   toggleTheme() {
     this.updateTheme();
   }
   updateTheme() {
-    switch (this.themeSwitcher.themeSwitchCounter) { // ignore this
-      case 10:
-        alert('bro stop')
-        break;
-      case 20:
-        alert('seriously')
-        break;
-      case 30:
-        alert('stop it')
-        break;
-      case 40:
-        alert('you\'re gonna break it')
-        break;
-      case 50:
-        document.body.className = 'what'
-        alert('great. you broke it.')
-        return;
-      case 60:
-        alert('it\'s broken')
-        return;
-      case 70:
-        alert('it\'s still broken')
-        return;
-      case 80:
-        alert('it\'s no use man. it\'s broken')
-        return;
-      case 90:
-        alert('you\'re still here?')
-        return;
-      case 100:
-        alert('fine. you win. i\'ll fix it.')
-        this.themeSwitcher.themeSwitchCounter = 0;
-        break;
-    } if (this.themeSwitcher.themeSwitchCounter > 50) return;
-
+    if (this.language === 'de') {
+      switch (this.themeSwitcher.themeSwitchCounter) { // ignore this
+        case 10:
+          alert('bro hör auf')
+          break;
+        case 20:
+          alert('ernsthaft')
+          break;
+        case 30:
+          alert('lass es bleiben')
+          break;
+        case 40:
+          alert('du machst es sonst kaputt')
+          break;
+        case 50:
+          document.body.className = 'what'
+          alert('super. jetzt ist\'s hin.')
+          return;
+        case 60:
+          alert('es ist kaputt')
+          return;
+        case 70:
+          alert('es ist immer noch kaputt')
+          return;
+        case 80:
+          alert('es hat keinen zweck, man. Es ist kaputt')
+          return;
+        case 90:
+          alert('immer noch da?')
+          return;
+        case 100:
+          alert('okay. du hast gewonnen. ich repariere\'s.')
+          this.themeSwitcher.themeSwitchCounter = 0;
+          break;
+      } if (this.themeSwitcher.themeSwitchCounter > 50) return;
+    }
+    else {
+      switch (this.themeSwitcher.themeSwitchCounter) { // ignore this
+        case 10:
+          alert('bro stop')
+          break;
+        case 20:
+          alert('seriously')
+          break;
+        case 30:
+          alert('stop it')
+          break;
+        case 40:
+          alert('you\'re gonna break it')
+          break;
+        case 50:
+          document.body.className = 'what'
+          alert('great. you broke it.')
+          return;
+        case 60:
+          alert('it\'s broken')
+          return;
+        case 70:
+          alert('it\'s still broken')
+          return;
+        case 80:
+          alert('it\'s no use man. it\'s broken')
+          return;
+        case 90:
+          alert('you\'re still here?')
+          return;
+        case 100:
+          alert('fine. you win. i\'ll fix it.')
+          this.themeSwitcher.themeSwitchCounter = 0;
+          break;
+      } if (this.themeSwitcher.themeSwitchCounter > 50) return;
+    }
 
     document.body.className = this.themeSwitcher.themeClass;
   }
