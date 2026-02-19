06b7247009693c055302c80ab05705a0c5277bc4
Ascyt <account@ascyt.com>
2026-02-19 21:28:57 +0100

    Copied style from projects to blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { LanguageValues } from '../language-values';
+import { ThemeSwitcherService } from '../theme-switcher/theme-switcher.service';
+
+interface Blog {
+  name: string;
+  description: string|null;
+
+  routerLink: string|null;
+
+  image: string|null;
+}
 
 @Component({
   selector: 'app-blog',
@@ -10,14 +21,29 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
   styleUrl: './blog.component.scss'
 })
 export class BlogComponent {
-  public constructor(private titleService: Title, private metaService: Meta, private activatedRoute: ActivatedRoute, private router:Router) {}
+  public get language():string {
+    return LanguageValues.language;
+  }
+
+  public get currentRoute():string {
+    return window.location.toString();
+  }
+
+  public constructor(private titleService: Title, private metaService: Meta, private activatedRoute: ActivatedRoute, private router:Router, public themeSwitcher: ThemeSwitcherService) {}
 
   ngOnInit(): void {
     this.activatedRoute.url.subscribe(url => {
-      this.titleService.setTitle('Ascyt • Blog');
-      this.metaService.updateTag({property: 'og:title', content: 'Ascyt • Blog'});
-      this.metaService.updateTag({property: 'og:url', content: 'https://ascyt.com' + this.router.url});
-      this.metaService.updateTag({property: 'og:description', content: 'Blog posts and articles from Ascyt (Filip Schauer).'});
+      this.titleService.setTitle((this.language === 'de' ? 'Ascyt • Blog' : 'Ascyt • Blog'));
+      this.metaService.updateTag({property: 'og:title', content: (this.language === 'de' ? 'Ascyt • Blog' : 'Ascyt • Blog')});
+      this.metaService.updateTag({property: 'og:url', content: (this.language === 'de' ? 'https://de.ascyt.com' : 'https://ascyt.com') + this.router.url});
+      this.metaService.updateTag({property: 'og:description', content: (this.language === 'de' ? 'Blogbeiträge und Artikel von Ascyt (Filip Schauer).' : 'Blog posts and articles from Ascyt (Filip Schauer).')});
     });
   }
+
+    blogs: Blog[] = this.language === 'de' ? [
+      {name: '[EN] A Guide to Fast Typing', description: 'A comprehensive guide to learning to type quickly on a computer keyboard.', routerLink: 'typing', image: 'assets/images/blog/typing/typing.png'},
+    ] : [
+      {name: 'A Guide to Fast Typing', description: 'A comprehensive guide to learning to type quickly on a computer keyboard.', routerLink: 'typing', image: 'assets/images/blog/typing/typing.png'},
+    ];
+  
 }
