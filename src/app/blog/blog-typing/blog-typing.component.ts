81baaa78ca462112f0f7c16b1f45b4395630ea38
Ascyt <account@ascyt.com>
2026-02-19 14:21:41 +0100

    Wrote typing blog

--- src/app/blog/blog-typing/blog-typing.component.ts
+++ src/app/blog/blog-typing/blog-typing.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BlogMetaInfo } from '../models/meta-info';
 import { DatePipe } from '@angular/common';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { Meta, Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-typing',
@@ -15,4 +17,15 @@ export class BlogTypingComponent {
     created: new Date("2026-02-18"),
     lastUpdated: new Date("2026-02-18")
   };
+
+  public constructor(private titleService: Title, private metaService: Meta, private activatedRoute: ActivatedRoute, private router:Router) {}
+  
+  ngOnInit(): void {
+    this.activatedRoute.url.subscribe(url => {
+      this.titleService.setTitle('A Guide To Fast Typing');
+      this.metaService.updateTag({property: 'og:title', content: 'A Guide To Fast Typing'});
+      this.metaService.updateTag({property: 'og:url', content: 'https://ascyt.com' + this.router.url});
+      this.metaService.updateTag({property: 'og:description', content: 'A comprehensive guide to learning to type quickly on a computer keyboard.'});
+    });
+  }
 }
